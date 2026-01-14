961a51eade61595d04000e1e28bae51147a1f17a
Re1oxy <artyrawanhot@gmail.com>
2026-01-14 22:34:06 +0100

    #Add csv data loader

---
+++ iris-authentication-system/core/data_loader.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+class DataLoader:
+	"""Loads CSV dataset for iris authentification."""
+
+	def __init__(self, train_path: str, test_path: str, label_column: str):
+		self.train_path = train_path
+		self.test_path = test_path
+		self.label_column = label_column
+
+	def load_train(self):
+		df = pd.read_csv(self.train_path)
+		X = df.drop(columns= [self.label_columns])
+		y = df[self.label_column]
+		return X, y
+
+	def load_test(self):
+		df = pd.read_csv(self.test_path)
+		X = df.drop(columns= [self.label_columns])
+		y = df[self.label_column]
+		return X, y

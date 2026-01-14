f814822798ab29330ca87267268489cbce9c6828
Re1oxy <artyrawanhot@gmail.com>
2026-01-14 22:59:57 +0100

    #Add evaluation metrics

---
+++ iris-authentication-system/core/evaluator.py
@@ -0,0 +1,12 @@
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+class Evaluator:
+    """Model evaluation utilities."""
+
+    @staticmethod
+    def evaluate(model, X_test, y_test):
+        predictions = model.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        matrix = confusion_matrix(y_test, predictions)
+        return accuracy, matrix

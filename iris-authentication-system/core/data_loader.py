e117fa67072ce99b4057cb98fd6ff2674d2266d4
Re1oxy <artyrawanhot@gmail.com>
2026-01-15 02:49:37 +0100

    #1 More mistake

--- iris-authentication-system/core/data_loader.py
+++ iris-authentication-system/core/data_loader.py
@@ -10,12 +10,12 @@ class DataLoader:
 
 	def load_train(self):
 		df = pd.read_csv(self.train_path)
-		X = df.drop(columns= [self.label_columns])
+		X = df.drop(columns= [0])
 		y = df[0]
 		return X, y
 
 	def load_test(self):
 		df = pd.read_csv(self.test_path)
-		X = df.drop(columns= [self.label_columns])
-		y = df[self.label_column]
+		X = df.drop(columns= [0])
+		y = df[0]
 		return X, y

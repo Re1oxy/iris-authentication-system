547720a8015003df520e9b8c813a67ac598122f2
Re1oxy <artyrawanhot@gmail.com>
2026-01-15 02:57:34 +0100

    #Cast label column to discrete classes

--- iris-authentication-system/core/data_loader.py
+++ iris-authentication-system/core/data_loader.py
@@ -6,16 +6,18 @@ class DataLoader:
 	def __init__(self, train_path: str, test_path: str, label_column: str):
 		self.train_path = train_path
 		self.test_path = test_path
-		self.label_column = label_column
+		self.label_column = str(label_column)
 
 	def load_train(self):
 		df = pd.read_csv(self.train_path)
-		X = df.drop(columns= [0])
-		y = df[0]
+		X = df.drop(columns= ['0'])
+		y = df[self.label_column].astype(int)
+
 		return X, y
 
 	def load_test(self):
 		df = pd.read_csv(self.test_path)
-		X = df.drop(columns= [0])
-		y = df[0]
+		X = df.drop(columns= ['0'])
+		y = df[self.label_column].astype(int)
+
 		return X, y
